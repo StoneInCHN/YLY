48f476ecb45a4861d09100deaa02c371e4fd3c95
yohu <82370288@qq.com>
2015-11-09 10:12:36 +0800

    missing check in

---
+++ yly-mgmt-pom/yly-mgmt-product/src/main/java/com/yly/service/MedicalRecordService.java
@@ -0,0 +1,8 @@
+package com.yly.service;
+
+import com.yly.entity.MedicalRecord;
+import com.yly.framework.service.BaseService;
+
+public interface MedicalRecordService extends BaseService<MedicalRecord, Long> {
+
+}
