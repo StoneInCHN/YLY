6d26af713959bce6ae16e91a32d31c351af5279d
sujinxuan <sujinxuan@CNPC0308-CD.cnt01.local>
2016-02-22 17:42:23 +0800

    fix system config

--- yly-mgmt-pom/yly-mgmt-product/src/main/java/com/yly/service/impl/SystemConfigServiceImpl.java
+++ yly-mgmt-pom/yly-mgmt-product/src/main/java/com/yly/service/impl/SystemConfigServiceImpl.java
@@ -24,66 +24,67 @@ import com.yly.utils.FieldFilterUtils;
 
 /**
  * 数据字典
+ * 
  * @author sujinxuan
  *
  */
 @Service("systemConfigServiceImpl")
-public class SystemConfigServiceImpl extends BaseServiceImpl<SystemConfig, Long> implements SystemConfigService {
+public class SystemConfigServiceImpl extends BaseServiceImpl<SystemConfig, Long> implements
+    SystemConfigService {
 
   @Resource(name = "systemConfigDaoImpl")
   private SystemConfigDao systemConfigDao;
-  
+
   @Resource
   public void setBaseDao(SystemConfigDao systemConfigDao) {
     super.setBaseDao(systemConfigDao);
   }
-  
+
   @Override
   public List<Map<String, Object>> findByConfigKey(ConfigKey configKey, Direction direction) {
-    if(configKey!=null){
+    if (configKey != null) {
       List<Filter> filters = new ArrayList<Filter>();
       List<Ordering> orderings = new ArrayList<Ordering>();
       Filter keyFilter = new Filter("configKey", Operator.eq, configKey);
-      Filter enableFilter = new Filter("isEnabled",Operator.eq,true);
+      Filter enableFilter = new Filter("isEnabled", Operator.eq, true);
       filters.add(enableFilter);
       filters.add(keyFilter);
-      if (direction!=null) {
+      if (direction != null) {
         Ordering ordering = new Ordering();
         ordering.setProperty("configOrder");
         ordering.setDirection(direction);
         orderings.add(ordering);
-     }
-     List<SystemConfig> systemConfigs = super.findList(null, filters, orderings,true,null);
-     if (systemConfigs == null || systemConfigs.size() == 0) {
-       filters.clear();
-       filters.add(keyFilter);
-       systemConfigs = super.findList(null,filters,null,true,null);
-       if (systemConfigs == null || systemConfigs.size() == 0) {
-         filters.clear();
-         filters.add(keyFilter);
-         systemConfigs = super.findList(null, filters, orderings);
-       }else {
-         return null;
-       }
-     }
-     String[] propertys =
-       {"id", "configValue"};
-     return FieldFilterUtils.filterCollectionMap(propertys, systemConfigs);
-   }
-   return null;
+      }
+      List<SystemConfig> systemConfigs = super.findList(null, filters, orderings, true, null);
+      // if (systemConfigs == null || systemConfigs.size() == 0) {
+      // filters.clear();
+      // filters.add(keyFilter);
+      // systemConfigs = super.findList(null, filters, null, true, null);
+      // if (systemConfigs == null || systemConfigs.size() == 0) {
+      // filters.clear();
+      // filters.add(keyFilter);
+      // systemConfigs = super.findList(null, filters, orderings);
+      // } else {
+      // return null;
+      // }
+      // }
+      String[] propertys = {"id", "configValue"};
+      return FieldFilterUtils.filterCollectionMap(propertys, systemConfigs);
+    }
+    return null;
   }
-  
+
   @Override
-  public Map<String,Object> getBillingDate(Date currentDate) {
-	  Map<String,Object> map = new HashMap<String, Object>();
+  public Map<String, Object> getBillingDate(Date currentDate) {
+    Map<String, Object> map = new HashMap<String, Object>();
     List<Map<String, Object>> dayList = findByConfigKey(ConfigKey.BILLDAY, null);
-    int billDay=0;
-    if(dayList!= null && dayList.size() ==1){
+    int billDay = 0;
+    if (dayList != null && dayList.size() == 1) {
       billDay = Integer.parseInt(dayList.get(0).get("configValue").toString());
       Date billDate = DateTimeUtils.getBillDate(currentDate, billDay);
-      map.put("billDate",DateTimeUtils.convertDateToString(billDate, "yyyy-MM-dd"));//账单结算日期
-      map.put("periodMonth",1);//最少缴费时间段为1个月
-      map.put("periodDay",DateTimeUtils.getBillDays(currentDate, billDay));//结算时需单独计算的天数
+      map.put("billDate", DateTimeUtils.convertDateToString(billDate, "yyyy-MM-dd"));// 账单结算日期
+      map.put("periodMonth", 1);// 最少缴费时间段为1个月
+      map.put("periodDay", DateTimeUtils.getBillDays(currentDate, billDay));// 结算时需单独计算的天数
       return map;
     }
     return null;
@@ -94,7 +95,7 @@ public class SystemConfigServiceImpl extends BaseServiceImpl<SystemConfig, Long>
     List<Filter> filters = new ArrayList<Filter>();
     Filter keyFilter = new Filter("configKey", Operator.eq, configKey);
     filters.add(keyFilter);
-    List<SystemConfig> systemConfigs = super.findList(null,filters,null,true,null);
+    List<SystemConfig> systemConfigs = super.findList(null, filters, null, true, null);
     return systemConfigs;
   }
 
